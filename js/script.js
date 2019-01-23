e7e8f95afbbd34011e533bcc05d139222bcadded
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-01-23 10:53:55 +0100

    Add css

--- js/script.js
+++ js/script.js
@@ -26,8 +26,19 @@ function generateTemplate(name, data, basicElement) {
 function showCountriesList(resp) {
     countriesList.innerHTML = '';
     resp.forEach(function (item) {
-        element = generateTemplate('country-template', { name: item.name, capital: item.capital, language: item.languages[0].name, currency: item.currencies[0].code, population: item.population,
-                    area: item.area, flag: item.flag});
+        let languages = '';
+        if (item.languages.length > 1) {
+            for (let i=0; i < item.languages.length; i++) {
+                languages +=item.languages[i].name;
+                if (i < item.languages.length - 1) {
+                    languages += ', ';
+                }
+            }
+        } else {
+            languages = item.languages[0].name;
+        }
+        element = generateTemplate('country-template', { name: item.name, capital: item.capital, language: languages, currency: (item.currencies[0].name + ' ' + item.currencies[0].symbol), 
+                    population: item.population/1000000, area: item.area/1000, flag: item.flag});
         countriesList.appendChild(element);
     });
 }
