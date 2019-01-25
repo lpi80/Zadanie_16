f097745e5c5566686c31801a58ff0b4df0185523
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-01-25 12:47:01 +0100

    Change let to const

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ button.addEventListener('click', function(){
 const paragraph = document.getElementById('joke');
 
 function getJoke() {
-    let xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.addEventListener('load', function(){
-      let response = JSON.parse(xhr.response);
+      const response = JSON.parse(xhr.response);
       paragraph.innerHTML = response.value.joke || 'Something went wrong';
     });
     xhr.send();
